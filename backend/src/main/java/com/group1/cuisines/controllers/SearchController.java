4dd1fe9af0d536cbf48a0f3a6203a40152857488
EnesBaserr <enesbaser8184@gmail.com>
2024-04-28 20:24:16 +0300

    Search route rearranged so that it takes query parameter.

--- backend/src/main/java/com/group1/cuisines/controllers/SearchController.java
+++ backend/src/main/java/com/group1/cuisines/controllers/SearchController.java
@@ -3,10 +3,7 @@ package com.group1.cuisines.controllers;
 import com.group1.cuisines.entities.Dish;
 import com.group1.cuisines.services.WikidataService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -15,8 +12,8 @@ import java.util.ArrayList;
 @RequiredArgsConstructor
 public class SearchController {
     private final WikidataService wikidataService;
-    @GetMapping("/dishes/{parameter}")
-    public ArrayList<Dish> searchDishes(@PathVariable String parameter) {
-        return wikidataService.retrieveDishAndCuisineData(parameter);
+    @GetMapping("/dishes")
+    public ArrayList<Dish> searchDishes(@RequestParam String q) {
+        return wikidataService.retrieveDishAndCuisineData(q);
     }
 }
