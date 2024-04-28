6923dd80ae821d5a73ad350cb5deb8c6f8076739
Mehmet Efe Akça <efeakca@gmail.com>
2024-04-28 23:55:56 +0300

    feat(frontend): add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { routes } from "./routes";
+
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    children: routes,
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-bold underline">Hell world!</h1>{" "}
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider
+      router={router}
+      fallbackElement={<p>Initial loading...</p>}
+    />
   );
 }
 
