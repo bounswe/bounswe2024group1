49686cfe9da9cb52a31cc80df72b2798885093bd
EnesBaserr <enesbaser8184@gmail.com>
2024-04-28 14:02:15 +0300

    Scheduling activated.

--- backend/src/main/java/com/group1/cuisines/CuisinesApplication.java
+++ backend/src/main/java/com/group1/cuisines/CuisinesApplication.java
@@ -2,8 +2,12 @@ package com.group1.cuisines;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableScheduling
+
 public class CuisinesApplication {
 
 	public static void main(String[] args) {
