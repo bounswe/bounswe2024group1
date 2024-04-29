3f1974c2bb6795c1d53a7de66964cf2f2419bc21
Yigit Memceroktay <yigitmemcer@gmail.com>
2024-04-29 18:21:38 +0300

    Add LoginPage

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,13 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { useFonts } from "expo-font";
 import LoginScreen from './src/screens/LoginScreen/LoginScreen';
-import Home from './src/screens/Home/Home';
+//import Home from './src/screens/Home/Home';
 export default function App() {
   
   return (
   
    <View className=" bg-white">
-      
+      <LoginScreen/>
     </View>
   );
 }
