f1bbada653b0f29f520324253b9841b23e64dc44
EnesBaserr <enesbaser8184@gmail.com>
2024-05-07 20:22:06 +0300

    users/me endpoint implemented.

---
+++ backend/src/main/java/com/group1/cuisines/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.group1.cuisines.controllers;
+
+import com.group1.cuisines.entities.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    @GetMapping("/me")
+    public ResponseEntity<?> getUserDetails(@AuthenticationPrincipal UserDetails userDetails) {
+        if (userDetails instanceof com.group1.cuisines.entities.User) {
+            User user = (User) userDetails;
+            Map<String, String> userInfo = new HashMap<>();
+            userInfo.put("username", user.getUsername());
+            userInfo.put("email", user.getEmail());
+            userInfo.put("bio", user.getBio());
+            userInfo.put("country", user.getCountry());
+
+            return ResponseEntity.ok(userInfo);
+        }
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User not authenticated");
+    }
+
+}
