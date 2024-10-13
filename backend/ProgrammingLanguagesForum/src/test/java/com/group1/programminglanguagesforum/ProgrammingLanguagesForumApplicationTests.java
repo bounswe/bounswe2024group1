ec15a8971a9bfb8e5598f6095ded4038b1382221
Atakan Yaşar <atakany357@gmail.com>
2024-10-13 20:11:43 +0300

    test: implement test controller

--- backend/ProgrammingLanguagesForum/src/test/java/com/group1/programminglanguagesforum/ProgrammingLanguagesForumApplicationTests.java
+++ backend/ProgrammingLanguagesForum/src/test/java/com/group1/programminglanguagesforum/ProgrammingLanguagesForumApplicationTests.java
@@ -1,13 +1,31 @@
 package com.group1.programminglanguagesforum;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProgrammingLanguagesForumApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testSetUp() throws Exception {
+		mockMvc.perform(get("/api/v1/test"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("test"));
 	}
 
 }
