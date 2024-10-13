c617faf4a0425bb145af209e2cea9c4f9152be3c
Atakan Yaşar <atakany357@gmail.com>
2024-10-13 19:52:41 +0300

    feat: initialize backend

---
+++ backend/ProgrammingLanguagesForum/src/test/java/com/group1/programminglanguagesforum/ProgrammingLanguagesForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.programminglanguagesforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingLanguagesForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
