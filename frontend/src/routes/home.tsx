6f17dc63f381cb9b902d8ebe33cfb34993d89a8b
Mehmet Efe Akça <efeakca@gmail.com>
2024-10-15 20:53:37 +0300

    fix: CI

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -1,3 +1,6 @@
+import { AnswerCard } from "@/components/AnswerCard";
+import { QuestionCard } from "@/components/QuestionCard";
+
 export function IndexRoute() {
   return (
     <>
@@ -5,6 +8,32 @@ export function IndexRoute() {
         <h1 className="mb-4 text-4xl font-bold">
           Welcome to Programming Languages Forum
         </h1>
+        <div className="grid grid-cols-3 gap-4">
+          <QuestionCard
+            id="q123"
+            title="Is there any algorithm to check memory safety and data races..."
+            content="I am aware of the question regarding combining AddressSanitizer and ThreadSanitizer. I am asking about this from the theoretical computer science point of..."
+            votes={4}
+            answerCount={2}
+            author={{
+              id: "u1",
+              name: "John Doe",
+              profilePicture: "https://placehold.co/400x400",
+            }}
+          />
+
+          <AnswerCard
+            id="a456"
+            title="Is there any algorithm to check memory safety and data races..."
+            content="Valgrind memcheck, DRD and Helgrind all detect errors in this code. For DRD I recommend using --tool=drd --fair-sched=yes --check-stack-var=yes..."
+            votes={1}
+            author={{
+              id: "u2",
+              name: "Jane Smith",
+              profilePicture: "https://placehold.co/400x400",
+            }}
+          />
+        </div>
       </div>
     </>
   );
