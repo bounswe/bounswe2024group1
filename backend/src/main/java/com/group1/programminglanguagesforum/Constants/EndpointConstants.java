418c854e1d02d77994c4932d9ed352a6b3f7d259
EnesBaserr <enesbaser8184@gmail.com>
2024-10-19 01:10:40 +0300

    Implemented user/me endpoint & also catching some exceptions thrown by Security Filter.

--- backend/src/main/java/com/group1/programminglanguagesforum/Constants/EndpointConstants.java
+++ backend/src/main/java/com/group1/programminglanguagesforum/Constants/EndpointConstants.java
@@ -2,7 +2,13 @@ package com.group1.programminglanguagesforum.Constants;
 
 public  class EndpointConstants {
     public static class AuthenticationEndpoints {
-        public static final String SIGNIN = "/auth/login";
-        public static final String SIGNUP = "/auth/signup";
+        public static final String  BASE_PATH = "/auth";
+        public static final String SIGNIN = BASE_PATH + "/login";
+        public static final String SIGNUP = BASE_PATH +"/signup";
+    }
+    public static class UserEndpoints {
+        public static final String  BASE_PATH = "/user";
+        public static final String USER_ME = BASE_PATH + "/me";
+        public static final String USER_ID = BASE_PATH + "{id}";
     }
 }
