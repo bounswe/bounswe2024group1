2262f1e2fb69f117bb8ca0b59584246d1f96750e
CagatayColak <cgtycolak@gmail.com>
2024-11-19 15:30:11 +0300

    Fix path

--- backend/src/main/java/com/group1/programminglanguagesforum/Constants/EndpointConstants.java
+++ backend/src/main/java/com/group1/programminglanguagesforum/Constants/EndpointConstants.java
@@ -1,16 +1,17 @@
 package com.group1.programminglanguagesforum.Constants;
 
-public  class EndpointConstants {
+public class EndpointConstants {
 
     public static final String TEST = "/test";
 
     public static class AuthenticationEndpoints {
-        public static final String  BASE_PATH = "/auth";
+        public static final String BASE_PATH = "/auth";
         public static final String SIGNIN = BASE_PATH + "/login";
-        public static final String SIGNUP = BASE_PATH +"/signup";
+        public static final String SIGNUP = BASE_PATH + "/signup";
     }
+
     public static class UserEndpoints {
-        public static final String  BASE_PATH = "/users";
+        public static final String BASE_PATH = "/users";
         public static final String USER_ME = BASE_PATH + "/me";
         public static final String USER_ID = BASE_PATH + "/{id}";
         public static final String USER_FOLLOW = BASE_PATH + "/{id}/follow";
@@ -35,14 +36,16 @@ public  class EndpointConstants {
     }
 
     public static class SparqlEndpoints {
-        public static final String  BASE_PATH = "https://query.wikidata.org/sparql";
+        public static final String BASE_PATH = "https://query.wikidata.org/sparql";
     }
+
     public static class TagEndpoints {
-        public static final String  BASE_PATH = "/tags";
+        public static final String BASE_PATH = "/tags";
         public static final String TAG_ID = BASE_PATH + "/{id}";
     }
+
     public static class BookmarkEndpoints {
-        public static final String  BASE_PATH = "/questions/{questionId}/bookmarks";
-        public static final String BOOKMARK_DELETE =  BASE_PATH + "/delete";
+        public static final String BASE_PATH = "/questions/{questionId}/bookmarks";
+        public static final String BOOKMARK_DELETE = BASE_PATH;
     }
 }
